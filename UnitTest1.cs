9079043b2819ce25fed7a95b794ac426e4d24a86
Matías de la Iglesia <mdelaiglesia@cys.com.ar>
2015-02-11 13:27:34 -0300

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NET
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        /**
+         * Rigourous Test :-)
+         */
+        [Test]
+        public void testApp()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void testAdd()
+        {
+            Assert.AreEqual(4, 2 + 2);
+        }
+
+        [Test]
+        public void testSubtract()
+        {
+            Assert.AreEqual(0, 2 - 2);
+        }
+    }
+}
